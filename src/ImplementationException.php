5436accf06666499864101c62a10e97c14cb4eaa
Mark Castillo <mark.castillo@makerbot.com>
2015-10-10 18:19:21 -0400

    First exception class for contract violations

---
+++ src/ImplementationException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Kastilyo\RabbitHole;
+
+use Exception;
+
+/**
+ * Thrown when an abstract method hasn't been implemented as expected
+ */
+class ImplementationException extends Exception
+{
+}
