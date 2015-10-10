e0994a0ab2ac2f1032c79e77099f6e376e3ae1d2
Mark Castillo <mark.castillo@makerbot.com>
2015-10-10 14:24:59 -0400

    Test helper managing stubs of third-party code

---
+++ spec/src/Helper.php
@@ -0,0 +1,25 @@
+<?php
+namespace Kastilyo\RabbitHole\Spec;
+
+use AMQPConnection;
+use kahlan\plugin\Stub;
+
+class Helper
+{
+    public static function initializeAMQPStubs()
+    {
+        Stub::on('AMQPChannel')->method('__construct');
+        Stub::on('AMQPExchange')->method('__construct');
+        Stub::on('AMQPExchange')->method('declareExchange');
+        Stub::on('AMQPQueue')->method('__construct');
+        Stub::on('AMQPQueue')->method('declareQueue');
+        Stub::on('AMQPQueue')->method('bind');
+    }
+    public static function getAMQPConnection()
+    {
+        return Stub::create([
+            'extends' => 'AMQPConnection',
+            'methods' => ['__construct', 'connect', 'isConnected'],
+        ]);
+    }
+}
