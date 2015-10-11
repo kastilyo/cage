f3d08103d849a823a456abd4b1f15f6b679d6cd1
Mark Castillo <mark.castillo@makerbot.com>
2015-10-10 22:36:49 -0400

    Docblocks for ExchangeBuilder

--- src/AMQP/ExchangeBuilder.php
+++ src/AMQP/ExchangeBuilder.php
@@ -5,12 +5,25 @@ use AMQPConnection;
 use AMQPChannel;
 use AMQPExchange;
 
+/**
+ * Responsible for declaring exchanges on a RabbitMQ and returning the
+ * AMQPExchange instance.
+ */
 class ExchangeBuilder
 {
     use ResourceBuilder;
 
+    /**
+     * Object cache of built exchanges indexed by name
+     * @var array
+     */
     private $exchanges = [];
 
+    /**
+     * Creates a fresh AMQPExchange instance, declaring it as a durable topic
+     * exchange with currently set name.
+     * @return \AMQPExchange
+     */
     private function build()
     {
         $exchange = new AMQPExchange($this->getChannel());
@@ -21,6 +34,10 @@ class ExchangeBuilder
         return $exchange;
     }
 
+    /**
+     * Lazily returns a fully-prepared AMQPExchange
+     * @return \AMQPExchange
+     */
     public function get()
     {
         $name = $this->getName();
@@ -31,6 +48,9 @@ class ExchangeBuilder
         return $this->exchanges[$name];
     }
 
+    /**
+     * Resets the state of the builder
+     */
     private function reset()
     {
         $this->name = null;
